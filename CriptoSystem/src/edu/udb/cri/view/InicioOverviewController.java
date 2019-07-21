530e3bc76e12fe0e48373480bd33ab0e7bab6189
luis <luis@luis.luis.chavez@desarrollador.net>
2019-07-21 16:48:12 -0600

    Nuevos cambios

--- CriptoSystem/src/edu/udb/cri/view/InicioOverviewController.java
+++ CriptoSystem/src/edu/udb/cri/view/InicioOverviewController.java
@@ -4,7 +4,6 @@ import java.net.URL;
 
 import javax.swing.JOptionPane;
 
-
 import edu.udb.cri.MainApp;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -30,14 +29,41 @@ public class InicioOverviewController {
 	private void initialize() {
 		// Initialize the start buttons
 
-		URL imgUrl = getClass().getResource("/simetric_cipher.png");
-		Image image = new Image(imgUrl.toString());
-		simetricoButton.setGraphic(new ImageView(image));
+		// Symmetric button
+		URL imgSimetric = getClass().getResource("/resources/simetric.png");
+		Image imageSimetric = new Image(imgSimetric.toString());
+		simetricoButton.setGraphic(new ImageView(imageSimetric));
 		simetricoButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Click en boton simetrico");
-            }
-        });
+			@Override
+			public void handle(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Click en boton simetrico");
+			}
+		});
+
+		// Asymmetric button
+		URL imgAsimetric = getClass().getResource("/resources/simetric.png");
+		Image imageAsimetric = new Image(imgAsimetric.toString());
+		asimetricoButton.setGraphic(new ImageView(imageAsimetric));
+		asimetricoButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Click en boton simetrico");
+			}
+		});
+
+		// Digital sing button
+		URL imgDs = getClass().getResource("/resources/simetric.png");
+		Image imageDs = new Image(imgDs.toString());
+		digitalSingButton.setGraphic(new ImageView(imageDs));
+		digitalSingButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Click en boton simetrico");
+			}
+		});
+	}
+
+	public void simetricCipher() {
 
 	}
 
