47300f5a0af05c0c82420a0125475b010be5a805
Luis Chavez <ljosue.chavez@gmail.com>
2019-07-21 08:21:29 -0600

    Agregando pantalla principal

---
+++ CriptoSystem/src/edu/udb/cri/MainApp.java
@@ -0,0 +1,66 @@
+package edu.udb.cri;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	
+	
+	private Stage primaryStage;
+    private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Criptografía");
+		initRootLayout();
+		showInicioOverview();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	/**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showInicioOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/InicioOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+            
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
