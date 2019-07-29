d04e668ed70197c6b2780ece316089173bd4e80d
lchavez <lchavez@lchavez.bdes.gob.sv>
2019-07-29 11:02:03 -0600

    Ultimos cambios

--- CriptoSystem/src/edu/udb/cri/view/ConfigurationViewController.java
+++ CriptoSystem/src/edu/udb/cri/view/ConfigurationViewController.java
@@ -1,5 +1,45 @@
 package edu.udb.cri.view;
 
+import java.io.File;
+
+import edu.udb.cri.MainApp;
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+
 public class ConfigurationViewController {
 
+	private MainApp mainApp;
+	
+	/**
+	 * Is called by the main application to give a reference back to itself.
+	 * 
+	 * @param mainApp
+	 */
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+	
+	  /**
+     * Opens a FileChooser to let the user select a file to save to.
+     */
+    @FXML
+    private void handleSaveAs() {
+        FileChooser fileChooser = new FileChooser();
+
+        // Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                "XML files (*.xml)", "*.xml");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Show save file dialog
+        File file = fileChooser.showSaveDialog(mainApp.getPrimaryStage());
+
+        if (file != null) {
+            // Make sure it has the correct extension
+            if (!file.getPath().endsWith(".xml")) {
+                file = new File(file.getPath() + ".xml");
+            }
+
+        }
+    }
 }
