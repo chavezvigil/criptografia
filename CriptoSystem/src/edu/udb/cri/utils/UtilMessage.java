5e21835548a8ffe6125020ba7c9ded8d10f68179
lchavez <lchavez@lchavez.bdes.gob.sv>
2019-07-31 14:54:41 -0600

    Cambios para utilmessage

--- CriptoSystem/src/edu/udb/cri/utils/UtilMessage.java
+++ CriptoSystem/src/edu/udb/cri/utils/UtilMessage.java
@@ -19,12 +19,12 @@ public class UtilMessage {
 
     /**
      * Devuelve un mensaje contenido en el archivo de propiedades para mensajes
-     * de la aplicaci�n.
+     * de la aplicacion.
      * @param llave Llave en el archivo de propiedades.
-     * @param argumentos Argumentos adicionales que reemplazar�n los marcadores
-     * de posici�n en el mensaje.
-     * @return Mensaje seg�n el archivo de propiedades con los marcadores de
-     * posici�n llenos seg�n los argumentos enviados en el �ltimo par�metro.
+     * @param argumentos Argumentos adicionales que reemplazaron los marcadores
+     * de posicion en el mensaje.
+     * @return Mensaje segun el archivo de propiedades con los marcadores de
+     * posicion llenos segun los argumentos enviados en el ultimo parametro.
      */
     public static String getMensaje(String llave, Object... argumentos) {
         String mensaje = rb.getString(llave);
