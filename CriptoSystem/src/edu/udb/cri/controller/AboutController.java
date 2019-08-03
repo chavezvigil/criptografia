005d113001898669f61c534e25c7d425afee7256
svejuarez <Lenovo@DESKTOP-M0DVFP3>
2019-08-03 09:52:23 -0600

    Update About view, por el momento he colocado iconos relacionados.

--- CriptoSystem/src/edu/udb/cri/controller/AboutController.java
+++ CriptoSystem/src/edu/udb/cri/controller/AboutController.java
@@ -1,43 +1,68 @@
 package edu.udb.cri.controller;
 
 import java.io.File;
+import java.net.URL;
+
+import javax.swing.JOptionPane;
+
+import edu.udb.cri.utils.UtilMessage;
+import javafx.application.Application;
 import javafx.application.HostServices;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
-public class AboutController {
-	public File file;
-	HostServices hostServices;
+public class AboutController extends Application{
+	private String rutRelativa = "recursos\\keystore\\Manual_Usuario.pdf";
+	private HostServices hostServices;
+	private File file;
 
     @FXML
     private Button btnGuiaUsuario;
-
+    
     @FXML
-    void getGuiaUsuario(ActionEvent event) {
-    	//FileChooser file = new FileChooser();
-    	//FileChooser.ExtensionFilter extFile = new FileChooser().getExtensionFilters();
-    	file = new File("/CriptoSystem/recursos/keystore/Manual_Usuario.pdf");
-    	//hostServices.showDocument(file.getAbsolutePath());
-    	//HostServices hostServices.getClass()
-    	//hostServices.showDocument(file.getAbsolutePath());
-        /*FileChooser fileChooser = new FileChooser();
-        
-        //Set extension filter
-        FileChooser.ExtensionFilter extFilter = 
-                new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf");
-        fileChooser.getExtensionFilters().add(extFilter);
-          
-        //Show open file dialog
-        File file = new File("/CriptoSystem/recursos/keystore/Manual_Usuario.pdf");*/
+    private ImageView imgLogo;
 
-        HostServices hostServices = getHostServices();
-        hostServices.showDocument(file.getAbsolutePath());
+	@FXML
+	private void initialize() {
+	    URL imgLogos = getClass().getResource(UtilMessage.getMensaje("edu.udb.cri.system.icon.logo"));
+	    Image imageLogos = new Image(imgLogos.toString());
+	    imgLogo.setImage(imageLogos);
+	    
+	    
+	    URL imgOpen = getClass().getResource(UtilMessage.getMensaje("edu.udb.cri.system.icon.openPdf"));
+	    Image imageOpen = new Image(imgOpen.toString());
+	    btnGuiaUsuario.setGraphic(new ImageView(imageOpen));	
+	}
+	
+    @FXML
+    void getGuiaUsuario(ActionEvent event) {   	
+    	file = new File(rutRelativa);
+    	try {
+			if (file.exists()) {
+		        hostServices = getHostServices();
+		        hostServices.showDocument(file.getAbsolutePath());
+			}else {
+				JOptionPane.showMessageDialog(null,"Archivo no existe, en ruta relativa");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
     }
 
-	private HostServices getHostServices() {
-		return hostServices;
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
 	}
+	
+	/*public AboutController(){
+	    URL imgDigital = getClass().getResource(UtilMessage.getMensaje("edu.udb.cri.system.icon.digital.sign"));
+	    Image imageDigital = new Image(imgDigital.toString());
+	    btnGuiaUsuario.setGraphic(new ImageView(imageDigital));
+	}*/
 
 
 }
