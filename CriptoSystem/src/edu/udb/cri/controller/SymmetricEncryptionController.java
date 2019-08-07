9784d33d394f75b9735d4336f002ff5695c65575
svejuarez <Administrator@Win-8Pro>
2019-08-07 10:25:47 -0600

    Update View + Controller Modulo simetrico

--- CriptoSystem/src/edu/udb/cri/controller/SymmetricEncryptionController.java
+++ CriptoSystem/src/edu/udb/cri/controller/SymmetricEncryptionController.java
@@ -1,117 +1,112 @@
 package edu.udb.cri.controller;
 
+import java.io.IOException;
+import java.util.function.UnaryOperator;
 import javax.swing.JOptionPane;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.scene.control.ToggleGroup;
+import edu.udb.cri.MainApp;
 import edu.udb.cri.utils.Symmetric;
+import edu.udb.cri.utils.UtilMessage;
 
 public class SymmetricEncryptionController {
     //Código interno que representa el algoritmo.
-    public final static int ALGORITMO_AES = 20;
-    public final static int ALGORITMO_DES = 22;
+    public static int ALGORITMO_AES = 20;
+    public static int ALGORITMO_DES = 22;
     private int flagAlg = 0;
     private int flagEnaProperty = 0;
+    private String inputStrModOpe = null;
+    private String inputStrTamBit = null;   
+
     
     @FXML
-    private TextField txtKeyDescifraAes;
-
+    private RadioButton rbCFBAes;
     @FXML
-    private ToggleGroup groupRbTamBitAes;
-
-    @FXML
-    private RadioButton rbOFBDes;
-
-    @FXML
-    private TextField txtKeyDescifraDes;
-
-    @FXML
-    private RadioButton rbECBDes;
-
-    @FXML
-    private TextField txtKeyCifraDes;
-
-    @FXML
-    private TextField txtInputSinAes;
-
-    @FXML
-    private TextField txtInputSinDes;
-
+    private RadioButton rbOFBAes;
     @FXML
     private RadioButton rbECBAes;
-
-    @FXML
-    private TextArea txtOutputConDes;
-
-    @FXML
-    private Button btnDecifradoDes;
-
     @FXML
     private RadioButton rb256Aes;
-
     @FXML
     private RadioButton rb192Aes;
-
-    @FXML
-    private TextField txtKeyCifraAes;
-
-    @FXML
-    private Button btnCifradoDes;
-
-    @FXML
-    private Button btnCifradoAes;
-
-    @FXML
-    private RadioButton rbCBCDes;
-
-    @FXML
-    private TextArea txtOutputSinDes;
-
-    @FXML
-    private TextArea txtOutputSinAes;
-
-    @FXML
-    private TextField txtVectorDes;
-
     @FXML
     private RadioButton rbCBCAes;
+    @FXML
+    private RadioButton rb128Aes;
+    @FXML
+    private RadioButton rbCFBDes;
+    @FXML
+    private RadioButton rbOFBDes;
+    @FXML
+    private RadioButton rbCBCDes;
+    @FXML
+    private RadioButton rbECBDes;   
 
     @FXML
     private TextField txtVectorAes;
-
     @FXML
-    private ToggleGroup groupRbModOpAes;
-
+    private TextField txtKeyCifraAes;
     @FXML
-    private RadioButton rb128Aes;
-
+    private TextField txtKeyDescifraAes;
+    @FXML
+    private TextField txtInputSinAes;
+    @FXML
+    private TextField txtKeyDescifraDes;
+    @FXML
+    private TextField txtVectorDes;
+    @FXML
+    private TextField txtInputSinDes;    
+    @FXML
+    private TextArea txtOutputSinDes;
+    @FXML
+    private TextField txtKeyCifraDes;
+    @FXML
+    private TextArea txtOutputConDes;
+    @FXML
+    private TextArea txtOutputConAes;
+    @FXML
+    private TextArea txtOutputSinAes;
+    
+    @FXML
+    private Button btnCifradoDes;
+    @FXML
+    private Button btnCifradoAes;
+    @FXML
+    private Button btnDecifradoDes;
     @FXML
     private Button btnDescifradoAes;
 
     @FXML
-    private ToggleGroup groupRbModOpDes;
+    private ToggleGroup groupRbModOpAes;    
+    @FXML
+    private ToggleGroup groupRbTamBitAes;
+    @FXML
+    private ToggleGroup groupRbModOpDes;    
 
     @FXML
-    private TextArea txtOutputConAes;
-
+    private Tab tabSelectAes;    
     @FXML
-    private RadioButton rbCFBDes;
-
-    @FXML
-    private RadioButton rbCFBAes;
-
-    @FXML
-    private RadioButton rbOFBAes;
+    private Tab tabSelectDes;
+    
     
     @FXML
     void EncryptionDes(ActionEvent event) {
 		String inputStrSinDes = txtInputSinDes.getText().toString();
 		String inputStrTamBit = "56";
-		String inputStrModOpe = null;
 		String inputStrVecIni = txtVectorDes.getText().toString();
 		String inputStrKeyCif = txtKeyCifraDes.getText().toString();
 		
@@ -126,20 +121,15 @@ public class SymmetricEncryptionController {
 	    		inputStrModOpe = rbOFBDes.getText();
 	    	}
 	    	
-	    	if (inputStrSinDes.length() != 0 && inputStrModOpe != null 
-	    			&& inputStrVecIni.length() != 0	&& inputStrKeyCif.length() != 0) {
-	    		if (inputStrVecIni != "ECB") {
-	    			Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_DES, inputStrVecIni);
-	    			txtOutputConDes.setText(objSimmControl.cifrar(inputStrSinDes,inputStrKeyCif,inputStrVecIni));
-	    		} else {
-	    			Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_DES, null);
-	    			txtOutputConDes.setText(objSimmControl.cifrar(inputStrSinDes,inputStrKeyCif,inputStrVecIni));
-	    		}
+	    	if (inputStrSinDes.length() != 0 && inputStrModOpe != null && inputStrKeyCif.length() != 0) {
+	    		Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_DES, inputStrVecIni);
+	    		txtOutputConDes.setText(objSimmControl.cifrar(inputStrSinDes,inputStrKeyCif,inputStrVecIni));
 	    		this.flagAlg = ALGORITMO_DES;
 	    		this.flagEnaProperty = 2;
 	    		disableProperty();
+	    		
 	    	}else {
-	    		JOptionPane.showMessageDialog(null,"Insertar texto cifrar y vector inicial");
+	    		JOptionPane.showMessageDialog(null,"Error: Falta llenar algun campo");
 	    	}			
 		} catch (Exception ex) {
 			JOptionPane.showMessageDialog(null,"Error: " + ex.getMessage());
@@ -150,7 +140,6 @@ public class SymmetricEncryptionController {
     void DecodeDes(ActionEvent event) {
 		String inputStrCifDes = txtOutputConDes.getText().toString();
 		String inputStrTamBit = "56";
-		String inputStrModOpe = null;
 		String inputStrVecIni = txtVectorDes.getText().toString();
 		String inputKeyDescifraDes = txtKeyDescifraDes.getText().toString();
 		
@@ -165,17 +154,13 @@ public class SymmetricEncryptionController {
 	    		inputStrModOpe = rbOFBDes.getText();
 	    	}
 	    	
-	    	if (inputStrCifDes.length() != 0 && inputStrModOpe != null 
-	    			&& inputKeyDescifraDes.length() != 0 && inputStrVecIni.length() !=0) {
-	    		if (inputStrModOpe != "ECB") {
-	    			Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_DES, inputStrVecIni);
-	    			txtOutputSinDes.setText(objSimmControl.descifrar(inputStrCifDes,inputKeyDescifraDes,inputStrVecIni));
-	    		} else {
-	    			Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_DES, null);
-	    			txtOutputSinDes.setText(objSimmControl.descifrar(inputStrCifDes,inputKeyDescifraDes,inputStrVecIni));
-	    		}
+	    	if (inputStrCifDes.length() != 0 && inputStrModOpe != null && inputKeyDescifraDes.length() != 0) 
+	    	{
+	    		Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_DES, inputStrVecIni);
+	    		txtOutputSinDes.setText(objSimmControl.descifrar(inputStrCifDes,inputKeyDescifraDes,inputStrVecIni));
+
 	    	}else {
-	    		JOptionPane.showMessageDialog(null,"Insertar texto cifrar y vector inicial");
+	    		JOptionPane.showMessageDialog(null,"Error: Falta ingresar clave para descifrado de " + inputStrTamBit + "bit");
 	    	}			
 		} catch (Exception ex) {
 			JOptionPane.showMessageDialog(null,"Error: " + ex.getMessage());
@@ -186,20 +171,10 @@ public class SymmetricEncryptionController {
     @FXML
     void EncryptionAes(ActionEvent event) {
 		String inputStrSinAes = txtInputSinAes.getText().toString();
-		String inputStrTamBit = null;
-		String inputStrModOpe = null;
 		String inputStrVecIni = txtVectorAes.getText().toString();
 		String inputStrKeyCif = txtKeyCifraAes.getText().toString();
 		
-		try {
-	    	if (rb128Aes.isSelected()) {
-	    		inputStrTamBit = rb128Aes.getText();
-	    	}else if (rb192Aes.isSelected()) {
-	    		inputStrTamBit = rb192Aes.getText();
-	    	}else if (rb256Aes.isSelected()) {
-	    		inputStrTamBit = rb256Aes.getText();
-	    	}
-	    	
+		try {	    	
 	    	if(rbCBCAes.isSelected()){
 	    		inputStrModOpe = rbCBCAes.getText();
 	    	}else if(rbCFBAes.isSelected()){
@@ -210,20 +185,16 @@ public class SymmetricEncryptionController {
 	    		inputStrModOpe = rbOFBAes.getText();
 	    	}
 	    	
-	    	if (inputStrSinAes.length() != 0 && inputStrVecIni.length() != 0 && inputStrKeyCif.length() != 0 
-	    			&& inputStrTamBit != null && inputStrModOpe != null) {
-	    		if(inputStrVecIni != "ECB") {
-	    			Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_AES, inputStrVecIni);
-	    			txtOutputConAes.setText(objSimmControl.cifrar(inputStrSinAes,inputStrKeyCif, inputStrVecIni));
-	    		}else {
-	    			Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_AES, null);
-	    			txtOutputConAes.setText(objSimmControl.cifrar(inputStrSinAes,inputStrKeyCif, inputStrVecIni));
-	    		}
+	    	if (inputStrSinAes.length() != 0 && inputStrKeyCif.length() != 0 && inputStrTamBit != null 
+	    			&& inputStrModOpe != null) 
+	    	{
+	    		Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_AES, inputStrVecIni);
+	    		txtOutputConAes.setText(objSimmControl.cifrar(inputStrSinAes,inputStrKeyCif, inputStrVecIni));
 	    		this.flagAlg = ALGORITMO_AES;
 	    		this.flagEnaProperty = 2;
 	    		disableProperty();
 	    	}else {
-	    		JOptionPane.showMessageDialog(null,"Error: No se ha insertar texto a cifrar, clave o vector de inicializacion");
+	    		JOptionPane.showMessageDialog(null,"Error: Falta llenar algun campo");
 	    	}			
 		} catch (Exception ex) {
 			JOptionPane.showMessageDialog(null,"Error: " + ex.getMessage());
@@ -233,20 +204,10 @@ public class SymmetricEncryptionController {
     @FXML
     void DecodeAes(ActionEvent event) {
 		String inputStrCifAes = txtOutputConAes.getText().toString();
-		String inputStrTamBit = null;
-		String inputStrModOpe = null;
 		String inputStrVecIni = txtVectorAes.getText().toString();
 		String inputStrKeyCif = txtKeyDescifraAes.getText().toString();
 		
-		try {
-	    	if (rb128Aes.isSelected()) {
-	    		inputStrTamBit = rb128Aes.getText();
-	    	}else if (rb192Aes.isSelected()) {
-	    		inputStrTamBit = rb192Aes.getText();
-	    	}else if (rb256Aes.isSelected()) {
-	    		inputStrTamBit = rb256Aes.getText();
-	    	}
-	    	
+		try {	    	
 	    	if(rbCBCAes.isSelected()){
 	    		inputStrModOpe = rbCBCAes.getText();
 	    	}else if(rbCFBAes.isSelected()){
@@ -257,18 +218,13 @@ public class SymmetricEncryptionController {
 	    		inputStrModOpe = rbOFBAes.getText();
 	    	}
 	    	
-	    	if (inputStrCifAes.length() != 0 && inputStrVecIni.length() != 0 && inputStrKeyCif.length() != 0 
-	    			&& inputStrTamBit != null && inputStrModOpe != null) {
-	    		if(inputStrVecIni != "ECB") {
+	    	if (inputStrCifAes.length() != 0 && inputStrKeyCif.length() != 0 && inputStrTamBit != null 
+	    			&& inputStrModOpe != null) {
 	    			Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_AES, inputStrVecIni);
 	    			txtOutputSinAes.setText(objSimmControl.descifrar(inputStrCifAes, inputStrKeyCif, inputStrVecIni));
-	    		}else {
-	    			Symmetric objSimmControl = new Symmetric(inputStrTamBit, inputStrModOpe, ALGORITMO_AES, null);
-	    			txtOutputSinAes.setText(objSimmControl.descifrar(inputStrCifAes,inputStrKeyCif, inputStrVecIni));
-	    		}
 	    		
 	    	}else {
-	    		JOptionPane.showMessageDialog(null,"Error: No se ha insertar texto a cifrar, clave o vector de inicializacion");
+	    		JOptionPane.showMessageDialog(null,"Error: Falta ingresar clave para descifrado de " + inputStrTamBit + "bit");
 	    	}			
 		} catch (Exception ex) {
 			JOptionPane.showMessageDialog(null,"Error: " + ex.getMessage());
@@ -296,7 +252,11 @@ public class SymmetricEncryptionController {
 	    	if ( this.flagAlg == 20) {
 	        	txtInputSinAes.setDisable(false);
 	        	txtKeyCifraAes.setDisable(false);
-	        	txtVectorAes.setDisable(false);
+	        	if (inputStrModOpe.contentEquals("ECB")) {
+	        		txtVectorAes.setDisable(true);
+	        	}else {
+	        		txtVectorAes.setDisable(false);
+	        	}
 	        	rbECBAes.setDisable(false);
 	        	rbECBAes.setDisable(false);
 	        	rbCBCAes.setDisable(false);
@@ -308,7 +268,11 @@ public class SymmetricEncryptionController {
 	    	}else if(this.flagAlg == 22) {
 	        	txtInputSinDes.setDisable(false);
 	        	txtKeyCifraDes.setDisable(false);
-	        	txtVectorDes.setDisable(false);
+	        	if (inputStrModOpe.contentEquals("ECB")) {
+	        		txtVectorDes.setDisable(true);
+	        	}else {
+	        		txtVectorDes.setDisable(false);  
+	        	}
 	        	rbECBDes.setDisable(false);
 	        	rbECBDes.setDisable(false);
 	        	rbCBCDes.setDisable(false);
@@ -343,6 +307,107 @@ public class SymmetricEncryptionController {
 		}
     	
     	return 0;
+    }
+    
+    
+    public static void limitTextField(TextField textField, int limit) {
+        UnaryOperator<Change> textLimitFilter = change -> {
+            if (change.isContentChange()) {
+                int newLength = change.getControlNewText().length();
+                if (newLength > limit) {
+                    String trimmedText = change.getControlNewText().substring(0, limit);
+                    change.setText(trimmedText);
+                    int oldLength = change.getControlText().length();
+                    change.setRange(0, oldLength);
+                }
+            }
+            return change;
+        };
+        textField.setTextFormatter(new TextFormatter<Object>(textLimitFilter));
+    } 
+    
+	@FXML
+	private void initialize() {
+		this.flagAlg = ALGORITMO_DES;
+		inputStrTamBit = rb128Aes.getText();
+		limitTextField(txtKeyCifraAes, 16);
+		limitTextField(txtKeyCifraDes, 8);
+		txtVectorDes.setDisable(true);
+		txtVectorAes.setDisable(true);
+	}
+    
+    @FXML
+    void radioSelect(ActionEvent event) {
+    	if (this.flagAlg == 22) {
+	    	txtKeyCifraDes.setText("");
+	    	txtVectorDes.setText("");
+	    	txtOutputConDes.setText("");
+	    	txtKeyDescifraDes.setText("");
+	    	txtOutputSinDes.setText("");
+	    	
+	    	
+	    	if(rbECBDes.isSelected()){
+    			txtVectorDes.setDisable(true);
+    			limitTextField(txtVectorDes, 8);
+	    	}else if(rbCBCDes.isSelected()){
+	    		txtVectorDes.setDisable(false);
+    			limitTextField(txtVectorDes, 8);
+	    	}else if(rbCFBDes.isSelected()){
+	    		txtVectorDes.setDisable(false);
+    			limitTextField(txtVectorDes, 8);
+	    	}else if(rbOFBDes.isSelected()){
+	    		txtVectorDes.setDisable(false);
+    			limitTextField(txtVectorDes, 8);
+	    	}
+    	}else {
+	    	txtKeyCifraAes.setText("");
+	    	txtVectorAes.setText("");
+	    	txtOutputConAes.setText("");
+	    	txtKeyDescifraAes.setText("");
+	    	txtOutputSinAes.setText("");
+	    	
+	    	if (rb128Aes.isSelected()) {
+	    		inputStrTamBit = rb128Aes.getText();
+	    		limitTextField(txtKeyCifraAes, 16);
+	    		limitTextField(txtVectorAes, 16);
+	    	}else if (rb192Aes.isSelected()) {
+	    		inputStrTamBit = rb192Aes.getText();
+	    		limitTextField(txtKeyCifraAes, 24);
+	    		limitTextField(txtVectorAes, 16);
+	    	}else if (rb256Aes.isSelected()) {
+	    		inputStrTamBit = rb256Aes.getText();
+	    		limitTextField(txtKeyCifraAes, 32);
+	    		limitTextField(txtVectorAes, 16);
+	    	}
+	    	
+	    	if(rbECBAes.isSelected()){
+    			txtVectorAes.setDisable(true);
+	    	}else if(rbCBCAes.isSelected()){
+	    		txtVectorAes.setDisable(false);
+	    	}else if(rbCFBAes.isSelected()){
+	    		txtVectorAes.setDisable(false);
+	    	}else if(rbOFBAes.isSelected()){
+	    		txtVectorAes.setDisable(false);
+	    	}
+    	}
+    }
+    
+    @FXML
+    void openTabAes(Event event) {
+    	String tbCifrado = tabSelectAes.getText().toString();
+    	String strAlgoritmo = tbCifrado.substring(8, 11);		
+    	if(strAlgoritmo.contentEquals("AES")) {
+    		this.flagAlg = ALGORITMO_AES;
+    	}
+    }
+    
+    @FXML
+    void openTabDes(Event event) {
+    	String tbCifrado = tabSelectDes.getText().toString();
+    	String strAlgoritmo = tbCifrado.substring(8, 11);		
+    	if(strAlgoritmo.contentEquals("DES")) {
+    		this.flagAlg = ALGORITMO_DES;
+    	}
     }
     
 }
